f7fac434a44c1791235123d5e514aaba47c610d2
ZikannSenpai <zidanriski778@gmail.com>
2026-02-13 15:37:23 +0700

    reset

--- components/ui/Container.tsx
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto max-w-7xl px-3 sm:px-4 md:px-6 lg:px-8 xl:px-12">{children}</div>;
-}
