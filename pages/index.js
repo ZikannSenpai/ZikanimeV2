d092ffe1eb427955d266df0420c686d90bd11ebd
ZikannSenpai <zidanriski778@gmail.com>
2026-02-22 01:12:34 +0700

    up

--- pages/index.js
+++ pages/index.js
@@ -31,15 +31,33 @@ export default function Home() {
         load();
     }, []);
 
-    if (loading) return <div>Loading...</div>;
+    if (loading)
+        return (
+            <div className="loading">
+                <div className="spinner"></div>
+            </div>
+        );
 
     return (
-        <div>
-            {anime.map(item => (
-                <div key={item.animeId}>
-                    <h3>{item.title}</h3>
-                </div>
-            ))}
+        <div className="container">
+            <div className="anime-grid">
+                {anime.map(item => (
+                    <div key={item.animeId} className="anime-card fade-in">
+                        <img
+                            src={item.poster}
+                            alt={item.title}
+                            className="anime-poster"
+                        />
+                        <div className="anime-info">
+                            <h3 className="anime-title">{item.title}</h3>
+                            <div className="anime-meta">
+                                <span>{item.episode}</span>
+                                <span className="anime-type">{item.type}</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
